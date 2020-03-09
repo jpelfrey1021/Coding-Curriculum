b9d5f91c29d9f351b802a6fe9dcc864c1f817bcc
Jessica Pelfrey <jpelfrey@redventures.com>
2020-03-09 10:38:30 -0400

    submitting week 4

--- 01_Core/week04/submissions/Contact-us/contact-script.js
+++ 01_Core/week04/submissions/Contact-us/contact-script.js
@@ -1,19 +1,11 @@
-var phone = document.forms["contact-form"]["phone"].value
-var email = document.forms["contact-form"]["email"].value
-var message = document.forms["contact-form"]["customer-message"].value
-
-console.log(email)
-
 function contactUs() {
-    if (email== "") {
-        alert("Must enter an email or phone number")
-        console.log(phone)
-        return false
-    } else if (message == "") {
-        alert("Must enter a message")
-        return false
+    var phone = document.forms["contact-form"]["phone"].value;
+    var email = document.forms["contact-form"]["email"].value;
+    var message = document.forms["contact-form"]["customer-message"].value;
+
+    if (message == "" || email == "" || phone == "") {
+        alert("Erro: input field is empty")
     } else {
-        alert("Form Submitted Successfully")
-        return true;
+        alert("Form submitted successfully");
     }
 }

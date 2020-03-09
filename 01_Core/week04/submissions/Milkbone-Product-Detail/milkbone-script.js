1521f4fd1fbd002814529935514e02b3e9001e32
Jessica Pelfrey <jpelfrey@redventures.com>
2020-03-08 21:18:46 -0400

    adding week 4 submissions

---
+++ 01_Core/week04/submissions/Milkbone-Product-Detail/milkbone-script.js
@@ -0,0 +1,15 @@
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("images__shown");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
